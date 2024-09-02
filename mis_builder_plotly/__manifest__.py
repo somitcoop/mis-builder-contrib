7e57548f7c27793d3f559b37c924dc8cbbe7584d
Jose Robles <jose.robles@somit.coop>
2024-09-02 16:23:52 +0200

    [IMP] mis_builder_plotly: Added a unique constraint for KPI and Report instance combination, added a new view for KPI, updated KPI form view to include related fields, and improved the KPI tree view.

--- mis_builder_plotly/__manifest__.py
+++ mis_builder_plotly/__manifest__.py
@@ -20,6 +20,7 @@
         "security/ir.model.access.csv",
         "views/mis_report_plotly.xml",
         "views/mis_report_instance.xml",
+        "views/mis_report_instance_kpi.xml",
         "views/mis_report_instance_period.xml",
         "views/mis_report_style.xml",
         "views/mis_report.xml",
--- mis_builder_plotly/models/mis_report_instance_kpi.py
+++ mis_builder_plotly/models/mis_report_instance_kpi.py
@@ -4,13 +4,31 @@ from odoo import models, fields, api, _
 class MisReportInstanceKpi(models.Model):
     _name = "mis.report.instance.kpi"
 
+    _sql_constraints = [
+        (
+            "unique_kpi_report_instance_combination",
+            "UNIQUE(mis_report_instance_id,kpi_id)",
+            _("Kpi must be unique per Report instance"),
+        )
+    ]
+
 
     mis_report_instance_id = fields.Many2one(
         'mis.report.instance',
         string="Report Instance",
+        required=True
+    )
+    report_kpi_ids = fields.One2many(
+        'mis.report.kpi',
+        related="mis_report_instance_id.report_id.kpi_ids"
     )
     kpi_id = fields.Many2one(
         'mis.report.kpi',
-        domain=[('id', 'in', 'mis_report_instance_id.report_id.kpi_ids')],
+        domain="[('id', 'in', report_kpi_ids)]",
         ondelete="cascade",
+        required=True
+    )
+    kpi_style_id = fields.Many2one(
+        'mis.report.plotly.style',
+        related="kpi_id.plotly_style_id"
     )
